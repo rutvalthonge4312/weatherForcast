1b83c194a6a9828d6901b98c7085d62f54dcdb2b
rutvalthonge4312 <127858102+rutvalthonge4312@users.noreply.github.com>
2023-09-23 16:27:51 +0530

    sencond update

--- index.js
+++ index.js
@@ -4,8 +4,26 @@ let humidityPer = document.getElementById("humidityPer");
 let location12 = document.getElementById("location12");
 let buttons = document.getElementById("buttons");
 let currentDate = document.getElementById("currentDate");
+let iconWeather = document.getElementById("iconWeather");
+let currentTime = document.getElementById("currentTime");
 let descriptionWeathoe = document.getElementById("descriptionWeathoe");
+let maxTemp = document.getElementById("maxTemp");
+let minTemp = document.getElementById("minTemp");
+let duplicateDesc = document.getElementById("duplicateDesc");
+let feelsLike = document.getElementById("feelsLike");
 const apiKey = "074e3cf34f29d282a0d174bf7f90581b";
+
+function displayTime() {
+  currentTime.innerHTML = new Date().toLocaleTimeString();
+}
+// let time = `${date.getHours()}:${date.getMinutes()}`;
+
+setInterval(displayTime, 1000);
+
+location12.addEventListener("click", function () {
+  location12.style.border = "1px solid grey";
+});
+
 showDetail();
 function showDetail() {
   let inputVal = location12.value;
@@ -13,6 +31,7 @@ function showDetail() {
   let day = date.getDate();
   let month = date.getMonth() + 1;
   let year = date.getFullYear();
+
   let cDate = `${day}-${month}-${year}`;
 
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
@@ -22,23 +41,23 @@ function showDetail() {
   fetch(request)
     .then((response) => response.json())
     .then((responseJson) => {
-      //var y = document.createTextNode(responseJson.wind);
-      // para.appendChild(y);
-      console.log(responseJson);
-      /*/
-      temparature.appendChild(document.createTextNode(responseJson.main.temp));
-      humidityPer.appendChild(
-        document.createTextNode(responseJson.main.humidity)
-      );
-      windSpeed.appendChild(document.createTextNode(responseJson.wind.speed));
-      descriptionWeathoe.appendChild(
-        document.createTextNode(responseJson.weather[0].description)
-      );*/
-      temparature.innerHTML = responseJson.main.temp + "&degc";
-      humidityPer.innerHTML = responseJson.main.humidity + "%";
-      windSpeed.innerHTML = responseJson.wind.speed + " mps";
-      descriptionWeathoe.innerHTML = responseJson.weather[0].description;
-      currentDate.innerHTML = cDate;
+      if (responseJson.cod == 404) {
+        alert("City Not Found");
+      } else {
+        console.log(responseJson);
+        maxTemp.innerHTML = responseJson.main.temp_max + "&degc";
+        minTemp.innerHTML = responseJson.main.temp_min + "&degc";
+        temparature.innerHTML = responseJson.main.temp + "&degc";
+        humidityPer.innerHTML = responseJson.main.humidity + "%";
+        windSpeed.innerHTML = responseJson.wind.speed + " mps";
+        descriptionWeathoe.innerHTML = responseJson.weather[0].description;
+        duplicateDesc.innerHTML = responseJson.weather[0].description;
+        currentDate.innerHTML = cDate;
+        let iconVal = responseJson.weather[0].icon;
+        feelsLike.innerHTML =
+          "Feels Like " + responseJson.main.feels_like + "&degc";
+        iconWeather.src = `https://openweathermap.org/img/wn/${iconVal}.png`;
+      }
     })
     .catch((err) => {
       console.log(err);
